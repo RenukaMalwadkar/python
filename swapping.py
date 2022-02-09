cbc3529263d49ad886319da855b776366e22dea3
RenukaMalwadkar <98079261+RenukaMalwadkar@users.noreply.github.com>
2022-02-09 16:19:21 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,17 @@
+#swapping using temp variable
+# x=12
+# y=21
+# x=input('Enter the value of x: ')
+# y=input('Enter the value of y: ')
+# temp=x
+# x=y
+# y=temp
+# print('THe value of x: {}'.format(x))
+# print('The value of y: {}'.format(y))
+
+#swapping without using temp variable
+x=11
+y=22
+x,y=y,x
+print("x=",x)
+print("y=",y)
